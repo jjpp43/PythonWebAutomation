cd7fc8b342352bc495c63912998b17a3971d5c49
jjpp43 <61809797+jjpp43@users.noreply.github.com>
2020-04-16 11:55:37 +0900

    AutoAttendence.py
    I have made an automated python program so that I could attend my online classes automatically. I have made this py file to exe format and added to the windows task scheduler. By doing so, I can attend my online classes while I am in bed. :)

--- YoutubeSelenium.py
+++ YoutubeSelenium.py
@@ -6,17 +6,18 @@ import time
 driver = webdriver.Chrome(executable_path='C:/users/junna/chromedriver.exe')
 url = 'https://blackboard.angelo.edu'
 driver.get(url)
-#창 크게 열기
+#Maximize window
 driver.maximize_window()
 
-#Agree & Continue
+#Click Agree & Continue button
 driver.find_element_by_id('agree_button').click()
 
-#Type in id
+#Type in my id
 id = driver.find_element_by_id('user_id')
-id.send_keys('jpark43')
+id.send_keys('myID')  #I have hidden my id for security purposes
+#Type in my password
 pw = driver.find_element_by_id('password')
-pw.send_keys('qkrwnsgk7#')
+pw.send_keys('myPassword')  #I have hidden my password for security purposes
 
 #Click login
 driver.find_element_by_id('entry-login').click()
@@ -24,8 +25,8 @@ driver.find_element_by_id('entry-login').click()
 #Wait for 2 seconds
 time.sleep(2)
 
-#Click CS3304
+#Click my CS3304 course
 driver.find_element_by_xpath('//*[@id="_4_1termCourses_noterm"]/ul/li[1]/a').click()
 
-#Click collaborate session tab
+#Click collaborate session tab, and now I am in my online class :)
 driver.find_element_by_xpath('//*[@id="paletteItem:_786592_1"]/a/span').click()
